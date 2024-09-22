b278cb8693abb7e0b2c26d52b7dd2bf06e753ed9
Andrey Shirokinskiy <ashirokinskiy@gmail.com>
2024-09-23 02:56:37 +0300

    Исправил код и сделал unit-тесты

---
+++ src/main/java/ru/andshir/CurrencyCountryParser.java
@@ -0,0 +1,44 @@
+package ru.andshir;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CurrencyCountryParser {
+
+    public static void main(String[] args) {
+        String input = "Ангола#AOA/Аргентина#ARS/Армения#AMD/Аруба#AWG/" +
+                "Афганистан#AFN/Багамские острова#BSD/Бангладеш#BDT/" +
+                "Барбадос#BBD/Бахрейн#BHD/Беларусь#BYR/Белиз#BZD/" +
+                "Бермудские острова#BMD/Болгария#BGN/Боливия#BOB/" +
+                "Босния и Герцеговина#BAM/Ботсвана#BWP/Бразилия#BRL/" +
+                "Бруней#BND/Бурунди#BIF/Бутан#BTN/Вануату#VUV/Великобритания#GBP/" +
+                "Венгрия#HUF/Венесуэла#VEB/Восточный Тимор; Индонезия#IDR/" +
+                "Вьетнам#VND/Габон; Камерун; Конго; Центрально-Африканская Республика; Чад; Экваториальная Гвинея#XAF/" +
+                "Гаити#HTG/Гайана#GYD/Гамбия#GMD/Колумбия#COP/Коморские острова#KMF/Коста-Рика#CRC/Куба#CUP";
+
+        CurrencyCountryParser ccp = new CurrencyCountryParser();
+
+        Map<String, String> currencyCountryMap = ccp.makeCurrencyCountryMap(input);
+        for (String key: currencyCountryMap.keySet()) {
+            System.out.println("Key: " + key + "; Value: " + currencyCountryMap.get(key));
+        }
+        System.out.println("\nMap size: " + currencyCountryMap.size());
+    }
+
+    /* If there is more than one country corresponding to the currency code, the value for the respective
+    key (currency code) will be a String containing all the corresponding countries separated by semicolon and space*/
+    private Map<String, String> makeCurrencyCountryMap(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("Provided argument is null");
+        } else if (!input.matches("([А-Я].*#[A-Z]{3}/)*([А-Я].*#[A-Z]{3})")) {
+            throw new IllegalArgumentException("Input string does not match the supported pattern");
+        } else {
+            Map<String, String> resultingMap = new HashMap<>();
+            Arrays.stream(input.split("/"))
+                    .map(stringArray -> stringArray.split("#"))
+                    .forEach(stringArray -> resultingMap.put(stringArray[1], stringArray[0]));
+            return resultingMap;
+        }
+    }
+}
